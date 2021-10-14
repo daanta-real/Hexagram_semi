6e22f53dc69f33c1260afd8ed391246a77d87295
hongbba <88869198+hongbba@users.noreply.github.com>
2021-10-14 13:48:20 +0900

    event_detail 제약조건 not null 추가 테스트

--- workspace/hongbba/이벤트.sql
+++ workspace/hongbba/이벤트.sql
@@ -2,6 +2,6 @@ create table event(
 event_idx number(20) primary key,
 user_idx references user_idx,
 event_subject varchar2(100) not null,
-event_detail varchar2(2000),
+event_detail varchar2(2000) not null,
 event_ables varchar2(200)
 );
