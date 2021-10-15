ba94b02fe00ead6471f50a3b75317d172b1097a7
hongbba <88869198+hongbba@users.noreply.github.com>
2021-10-16 01:52:53 +0900

    코스테이블 제약조건 추가

--- workspace/DB/코스테이블.sql
+++ workspace/DB/코스테이블.sql
@@ -1,7 +1,7 @@
 
 create table course(
 course_id NUMBER(30) primary key,
-user_idx NUMBER(20) references user(user_idx),
+user_idx NUMBER(20) references user(user_idx) on delete cascade, --회원이 탈퇴하면 글이 삭제되야 한다.
 course_list VARCHAR2(2000) not null,
 course_detail VARCHAR2(2000),
 course_locations VARCHAR2(2000) not null,
