cd08efbe722f5600234e707b45c63cab82419889
hongbba <88869198+hongbba@users.noreply.github.com>
2021-10-16 02:54:07 +0900

    코스테이블 수정

--- workspace/DB/코스테이블.sql
+++ workspace/DB/코스테이블.sql
@@ -10,6 +10,9 @@ course_tags VARCHAR2(2000)
 -- 코스테이블 시퀀스?
 create sequence course_seq;
 
--- 코스테이블 테스트 등록
+-- 코스테이블 테스트 등록(inster)
 insert into course(course_id, course_list, course_detail, course_locations, course_tags)
     values(course_seq.nextval, '목록(홍대-신촌-아현)', '내용(상상마당,각종유흥)', '지역(서울)', '태그(#홍대,#젊음의거리)');
+    
+-- 코스테이블 테스트 수정(update)
+update course set course_list='목록(홍대-신촌)', course_detail='내용(상상마당)', course_locations='지역(서울서울)', course_tags='태그(#홍대)';
