3d22046ce946a759ddce5e5b79aa76b7bfd6cb4d
hongbba <88869198+hongbba@users.noreply.github.com>
2021-10-16 01:44:18 +0900

    코스테이블 오타 수정

--- workspace/DB/코스테이블.sql
+++ workspace/DB/코스테이블.sql
@@ -1,7 +1,7 @@
 
 create table course(
 course_id NUMBER(30) primary key,
-user_idX NUMBER(20) references user(user_idx),
+user_idx NUMBER(20) references user(user_idx),
 course_list VARCHAR2(2000) not null,
 course_detail VARCHAR2(2000),
 course_locations VARCHAR2(2000) not null,
