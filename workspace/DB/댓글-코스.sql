7945d967bca93e6c95e8ab4da6cec001671d6fad
seona_min <88869283+seonamin@users.noreply.github.com>
2021-10-17 21:23:08 +0900

    user→users로 변경

--- workspace/DB/댓글-코스.sql
+++ workspace/DB/댓글-코스.sql
@@ -2,7 +2,7 @@
 create table reply_course(
 reply_idx NUMBER(20) primary key,
 course_idx NUMBER(20) references course(course_id),
-user_idx NUMBER(20) references user(user_idx),
+users_idx NUMBER(20) references users(users_idx),
 reply_detail VARCHAR2(210) not null,
 replay_target_id NUMBER(20)
 );
