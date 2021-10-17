5d57df9bb818b2a7bdc135f7272ee16199f38946
seona_min <88869283+seonamin@users.noreply.github.com>
2021-10-17 21:22:49 +0900

    user→users  /  reply_id→reply_idx 로 변경

--- workspace/DB/댓글-관광지.sql
+++ workspace/DB/댓글-관광지.sql
@@ -1,8 +1,8 @@
 
 create table reply_item(
-reply_id NUMBER(20) primary key,
+reply_idx NUMBER(20) primary key,
 item_idx NUMBER(20) references item(item_idx),
-user_idx NUMBER(20) references user(user_idx),
+users_idx NUMBER(20) references users(users_idx),
 reply_detail VARCHAR2(210) not null,
 reply_target_id NUMBER(20)
 );
