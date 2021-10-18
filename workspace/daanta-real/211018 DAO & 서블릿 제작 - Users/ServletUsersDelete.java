d4c19681feb1cd6fd5675e381bb5f8c76c03b501
Daanta <daanta@naver.com>
2021-10-19 06:48:07 +0900

    211019_0645 UsersDao 세션개념 적용, 라이브러리 작성
    1. UsersDao 세션개념 및 권한체크 적용
    - 수정(ServletUsersModify)
    - 삭제(ServletUsersDelete)
    
    2. 라이브러리 신설
    - Library: 일반 라이브러리
    - UsersUtils: Users 테이블용 라이브러리

--- workspace/daanta-real/211018 DAO & 서블릿 제작 - Users/ServletUsersDelete.java
+++ workspace/daanta-real/211018 DAO & 서블릿 제작 - Users/ServletUsersDelete.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,24 +8,48 @@ import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 @WebServlet("/usersUnregister")
-public class ServletUserDelete extends HttpServlet {
+public class ServletUsersDelete extends HttpServlet {
+
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException { try {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
 
-		req.setCharacterEncoding("UTF-8");
-		System.out.println("[회원 삭제]");
+			// 0. 설정
+			req.setCharacterEncoding("UTF-8");
+			resp.setContentType("text/html;charset=utf-8");
+			System.out.println("[회원 삭제]");
+			String id = req.getParameter("id");
+			System.out.println("0. 삭제 요청한 id: " + id);
 
-		// 값 받아옴
-		String id = req.getParameter("id");
+			// 1. 입력값 검사: 지울 id값이 들어와 있어야 됨
+			System.out.print("1. id 빈값 여부 확인..");
+			if(!Library.isExists(id)) throw new Exception();
+			System.out.println("id가 빈값이 아닌 것을 확인했습니다. 계속 진행합니다.");
 
-		// 빈값쳐내기
-		if(id == null || id.trim().length() == 0) { System.out.println("id 값이 입력되지 않았습니다."); return; }
-		// 값 검사: 양식에 맞지 않는 값은 쳐내기
-		// 작성 예정
+			// 2. 권한 검사: 해당 ID를 조작할 권한이 있는 상황인지 확인
+			System.out.print("2. 권한 확인..");
+			UsersDao dao = new UsersDao();
+			boolean isGranted = UsersUtils.chkIsGranted(req, dao, id);
+			if(!isGranted) throw new Exception();
+			System.out.println("권한 확인 완료.");
 
-		// 전송
-		System.out.println("회원삭제 요청: " + id);
-		System.out.println("회원삭제 결과: " + new UsersDao().delete(id));
+			// 3. 전송
+			System.out.print("3. 삭제 실행..");
+			boolean isSucceed = dao.delete(id);
+			System.out.println("　　▷ 삭제 요청 실시함.");
 
-	} catch (ClassNotFoundException | SQLException e) { e.printStackTrace(); } }
+			// 4. 결과 출력
+			System.out.print("4. 최종 결과: ");
+			if(isSucceed) {
+				System.out.println("삭제 성공.");
+			} else {
+				System.out.println("삭제 실패.");
+				throw new Exception();
+			}
+
+		} catch (Exception e) {
+			System.out.println("에러");
+			e.printStackTrace();
+		}
+	}
 }
--- workspace/daanta-real/211018 DAO & 서블릿 제작 - Users/ServletUsersModify.java
+++ workspace/daanta-real/211018 DAO & 서블릿 제작 - Users/ServletUsersModify.java
@@ -9,57 +9,63 @@ import javax.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
 @WebServlet("/usersModify")
 public class ServletUsersModify extends HttpServlet {
-@Override
-protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException { try {
 
-	// 설정
-	req.setCharacterEncoding("UTF-8");
-	System.out.println("[회원 수정]");
-	String id    = req.getParameter("id");
-	String pw    = req.getParameter("pw");
-	String nick  = req.getParameter("nick");
-	String email = req.getParameter("email");
-	String grade = req.getParameter("grade");
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
 
-	// DTO 제작 및 DAO 선언
-	System.out.print("1. DTO 내용: ");
-	UsersDto dto = new UsersDto();
-	if(id    != null) dto.setId   (id   );
-	if(pw    != null) dto.setPw   (pw   );
-	if(nick  != null) dto.setNick (nick );
-	if(email != null) dto.setEmail(email);
-	if(grade != null) dto.setGrade(grade);
-	UsersDao dao = new UsersDao();
-	System.out.println(dto);
+			// 0. 설정
+			req.setCharacterEncoding("UTF-8");
+			resp.setContentType("text/html;charset=utf-8");
+			System.out.println("[회원 수정]");
+			String id    = req.getParameter("id"   );
+			String pw    = req.getParameter("pw"   );
+			String nick  = req.getParameter("nick" );
+			String email = req.getParameter("email");
+			String grade = req.getParameter("grade");
 
-	// 입력값 검사: id랑 pw는 반드시 있어야 하고, nick/email/grade 셋중 하나 있어야 됨
-	System.out.print("2. 입력값 존재여부 검사..");
-	boolean filledReqs
-		=  (id != null && pw != null)
-		&& (nick != null || email != null || grade != null);
-	if(!filledReqs) throw new Exception();
-	System.out.println("필요 입력값 모두 존재.");
+			// 1. 입력값 검사: id는 무조건 있어야 되고, 추가로 nick/email/grade 셋중 하나는 있어야 됨
+			System.out.print("1. 입력값 존재여부 검사..");
+			boolean filledReqs
+				 =  Library.isExists(id)
+				&& (Library.isExists(nick) || Library.isExists(email) || Library.isExists(grade));
+			if(!filledReqs) throw new Exception();
+			System.out.println("필요 입력값 모두 존재.");
 
-	// id/pw 일치 검사
-	System.out.println("3. ID & PW 일치 검사..");
-	boolean idPwMatch = dao.idPwMatch(dto);
-	if(!idPwMatch) throw new Exception();
-	System.out.println("　　▷ 일치함.");
+			// 2. DTO 제작 및 DAO 선언
+			System.out.print("2. DTO 내용: ");
+			UsersDto dto = new UsersDto();
+			if(id    != null) dto.setId   (id   );
+			if(pw    != null) dto.setPw   (pw   );
+			if(nick  != null) dto.setNick (nick );
+			if(email != null) dto.setEmail(email);
+			if(grade != null) dto.setGrade(grade);
+			UsersDao dao = new UsersDao();
+			System.out.println(dto);
 
-	// 수정 실행
-	System.out.println("4. 실제 수정 실행..");
-	boolean isSucceed = dao.update(dto);
-	System.out.println("　　▷ 수정 요청 실시함.");
+			// 3. 권한 검사: 해당 ID를 조작할 권한이 있는 상황인지 확인
+			System.out.print("3. 권한 확인..");
+			boolean isGranted = UsersUtils.chkIsGranted(req, dao, id);
+			if(!isGranted) throw new Exception();
+			System.out.println("권한 확인 완료.");
 
-	// 결과출력
-	resp.setContentType("text/html;charset=utf-8");
-	System.out.print("5. 최종 결과: ");
-	if(isSucceed) {
-		System.out.println("수정 성공.");
-	} else {
-		System.out.println("수정 실패.");
-		throw new Exception();
+			// 4. 수정 실행
+			System.out.println("4. 실제 수정 실행..");
+			boolean isSucceed = dao.update(dto);
+			System.out.println("　　▷ 수정 요청 실시함.");
+
+			// 5. 결과 출력
+			System.out.print("5. 최종 결과: ");
+			if(isSucceed) {
+				System.out.println("수정 성공.");
+			} else {
+				System.out.println("수정 실패.");
+				throw new Exception();
+			}
+
+		} catch(Exception e) {
+			System.out.println(" ▷ 에러");
+			e.printStackTrace();
+		}
 	}
-
-} catch(Exception e) { System.out.println(" ▷ 에러"); e.printStackTrace(); } }
 }
