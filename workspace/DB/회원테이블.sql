683a030678a8467cdd830c0982243996be0d37d4
seona_min <88869283+seonamin@users.noreply.github.com>
2021-10-19 00:53:53 +0900

    SQL구문 수정

--- workspace/DB/회원테이블.sql
+++ workspace/DB/회원테이블.sql
@@ -4,8 +4,8 @@ users_id VARCHAR2(20) not null check(regexp_like(users_id, '^[a-z][a-z0-9-_]{4,1
 users_pw VARCHAR2(20) not null check(regexp_like(users_pw, '^[a-zA-Z0-9-_!@#$]{8,20}$')),
 users_nick VARCHAR2(30) not null,
 users_email VARCHAR2(30),
-users_phone char(13) not null check(regexp_like(users_phone, '^010-[0-9]{4}-[0-9]{4}$')),
-users_grade VARCHAR2(12) default '일반회원' not null check(users_grade in ('일반회원', '관리자'))
+users_phone char(13) check(regexp_like(users_phone, '^010-[0-9]{4}-[0-9]{4}$')),
+users_grade VARCHAR2(12) default '일반회원' check(users_grade in ('일반회원', '관리자'))
 );
 
 -- 시퀀스? 협의 필요
@@ -24,3 +24,8 @@ insert into users(users_idx, users_id, users_pw, users_nick, users_email, users_
 --users_pw : 필수입력정보. 8글자이상 20글자 미만.
 --users_phone : 필수입력정보. 형식이 정해져있는 문자열이라서 char로 설정  
 --users_grade : 등급미입력시 기본값 일반회원으로 설정
+
+select * from users;
+
+drop table users;
+drop sequence users_seq;
