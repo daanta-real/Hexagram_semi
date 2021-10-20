c1f4efc1ef8bcf6667f3c8681438e42bd9a35261
seona_min <88869283+seonamin@users.noreply.github.com>
2021-10-21 01:54:26 +0900

    예외전가 수정

--- src/main/java/beans/JdbcUtils.java
+++ src/main/java/beans/JdbcUtils.java
@@ -2,11 +2,10 @@ package beans;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class JdbcUtils {
 
-	public static Connection connect(String username, String password) throws ClassNotFoundException, SQLException {
+	public static Connection connect(String username, String password) throws Exception {
 		Class.forName("oracle.jdbc.OracleDriver");
 		Connection con = DriverManager.getConnection("jdeb:oracle:thin:@localhost:1521:xe", username, password);
 		return con;
