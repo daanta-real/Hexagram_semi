a7682babeb8914dd3ece7390cf68df1a5940c4c9
Daanta <daanta@naver.com>
2021-10-21 11:35:44 +0900

    211021_1135 JdbcUtils 내 주소 격리

--- src/main/java/beans/JdbcUtils.java
+++ src/main/java/beans/JdbcUtils.java
@@ -4,11 +4,13 @@ package beans;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import system.Settings;
+
 public class JdbcUtils {
 
 	public static Connection connect(String username, String password) throws Exception {
 		Class.forName("oracle.jdbc.OracleDriver");
-		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", username, password);
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@" + Settings.DBMS_ADDR + ":xe", username, password);
 		return con;
 	}
 }
