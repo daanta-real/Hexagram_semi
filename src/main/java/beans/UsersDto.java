60668a50b8b58cd0aeffcd3df08c98754d3e4740
Daanta <daanta@naver.com>
2021-10-26 10:21:24 +0900

    오기 수정

--- src/main/java/beans/UsersDto.java
+++ src/main/java/beans/UsersDto.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package beans;
 
 public class UsersDto {
@@ -53,50 +52,3 @@ public class UsersDto {
 	}
 
 }
-=======
-package beans;
-
-public class UsersDto {
-
-	private int usersIdx;			//회원번호
-	private String usersId;		//회원아이디
-	private String usersPw;		//회원비번
-	private String usersNick;		//회원닉네임
-	private String usersEmail;	//회원이메일
-	private String usersPhone;	//회원폰번
-	private String usersGrade;	//회원등급
-
-	//기본생성자
-	public UsersDto() {
-		super();
-	}
-
-	//getter
-	public int getUsersIdx() {		return usersIdx;		}
-	public String getUsersId() {		  return usersId;    	}
-	public String getUsersPw() { 		return usersPw;  	}
-	public String getUsersNick() {   	  return usersNick;		}
-	public String getUsersEmail() {		   return usersEmail;		}
-	public String getUsersPhone() {		return usersPhone;		}
-	public String getUsersGrade() {		return usersGrade;		}
-	public String getUsersEmailNull() {
-		if(usersEmail == null) return "미입력";
-		else return usersEmail;
-	}
-	public String getUsersPhoneNull() {
-		if(usersPhone == null) return "미입력";
-		else return usersPhone;
-	}
-
-	//setter
-	public void setUsersIdx(int usersIdx) {		this.usersIdx = usersIdx;		}
-	public void setUsersId(String usersId) {	  this.usersId = usersId;		}
-	public void setUsersPw(String usersPw) {		this.usersPw = usersPw;	 }
-	public void setUsersNick(String usersNick) { 		this.usersNick = usersNick;		}
-	public void setUsersEmail(String usersEmail) {	   this.usersEmail = usersEmail;		}
-	public void setUsersPhone(String usersPhone) {	  this.usersPhone = usersPhone;		}
-	public void setUsersGrade(String usersGrade) {		this.usersGrade = usersGrade;		}
-
-
-}
->>>>>>> branch 'main' of https://github.com/daanta-real/Hexagram_semi
