205dfacb4e583a00310e3a82b1f43665e96ffba7
seona_min <88869283+seonamin@users.noreply.github.com>
2021-10-27 18:22:28 +0900

    수정

--- src/main/java/servlet/users/UsersLoginServlet.java
+++ src/main/java/servlet/users/UsersLoginServlet.java
@@ -1,5 +1,23 @@
 package servlet.users;
 
-public class Test {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.UsersDto;
+import util.UsersUtils;
+
+@WebServlet(urlPatterns="/users/login.nogari")
+public class UsersLoginServlet extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+
+	
+	}
+	
 }
