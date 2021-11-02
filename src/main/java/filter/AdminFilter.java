2539b9fbc4cf09cff9aade34cc6ad53b5de858c0
Daanta <daanta@naver.com>
2021-11-03 01:23:19 +0900

    211103_0122 각 필터에 catch(Exception e) 부분 추가

--- src/main/java/filter/AdminFilter.java
+++ src/main/java/filter/AdminFilter.java
@@ -22,20 +22,29 @@ public class AdminFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-		// 준비
-		HttpServletRequest req = (HttpServletRequest)request;
-		System.out.println("[필터 작동 - 관리자 검사] from " + req.getRequestURL().toString());
+		try {
 
-		// 세션 검사 - grade가 정확히 관리자여야만 함.
-		String sessionGrade = (String) req.getSession().getAttribute("usersGrade");
-		if(!HexaLibrary.isExists(sessionGrade) && sessionGrade.equals("관리자")) {
-			System.out.println("[필터] 관리자가 아닙니다. (등급: '" + sessionGrade + "') 관리자 필터 통과 실패.");
-			((HttpServletResponse) response).sendError(401); // 로그인페이지로 이동
+			// 준비
+			HttpServletRequest req = (HttpServletRequest)request;
+			System.out.println("[필터 작동 - 관리자 검사] from " + req.getRequestURL().toString());
+
+			// 세션 검사 - grade가 정확히 관리자여야만 함.
+			String sessionGrade = (String) req.getSession().getAttribute("usersGrade");
+			if(!HexaLibrary.isExists(sessionGrade) && sessionGrade.equals("관리자")) {
+				System.out.println("[필터] 관리자가 아닙니다. (등급: '" + sessionGrade + "') 관리자 필터 통과 실패.");
+				((HttpServletResponse) response).sendError(401); // 로그인페이지로 이동
+			}
+
+			// 다음 필터로 넘김
+			System.out.println("[필터] 관리자가 맞습니다. 관리자 필터 통과.");
+			chain.doFilter(request, response);
+
+		} catch(Exception e) {
+
+			System.out.println("[필터] 처리 중에 에러 발생");
+			e.printStackTrace();
+
 		}
-
-		// 다음 필터로 넘김
-		System.out.println("[필터] 관리자가 맞습니다. 관리자 필터 통과.");
-		chain.doFilter(request, response);
 
 	}
 
--- src/main/java/filter/LoginFilter.java
+++ src/main/java/filter/LoginFilter.java
@@ -23,20 +23,28 @@ public class LoginFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-		// 준비
-		HttpServletRequest req = (HttpServletRequest)request;
-		System.out.println("[필터 작동 - 로그인 검사] from " + req.getRequestURL().toString());
+		try {
+			// 준비
+			HttpServletRequest req = (HttpServletRequest)request;
+			System.out.println("[필터 작동 - 로그인 검사] from " + req.getRequestURL().toString());
 
-		// 세션 검사
-		String sessionId = (String) req.getSession().getAttribute("usersId");
-		if(!HexaLibrary.isExists(sessionId)) {
-			System.out.println("[필터] 로그인되지 않아 로그인 검사 필터를 통과하지 못했습니다. (세션ID:'" + sessionId + "') 로그인 필터 통과 실패.");
-			((HttpServletResponse) response).sendError(401); // 로그인페이지로 이동
+			// 세션 검사
+			String sessionId = (String) req.getSession().getAttribute("usersId");
+			if(!HexaLibrary.isExists(sessionId)) {
+				System.out.println("[필터] 로그인되지 않아 로그인 검사 필터를 통과하지 못했습니다. (세션ID:'" + sessionId + "') 로그인 필터 통과 실패.");
+				((HttpServletResponse) response).sendError(401); // 로그인페이지로 이동
+			}
+
+			// 다음 필터로 넘김
+			System.out.println("[필터] 로그인 확인됨. 로그인 필터 통과.");
+			chain.doFilter(request, response);
+
+		} catch(Exception e) {
+
+			System.out.println("[필터] 처리 중에 에러 발생");
+			e.printStackTrace();
+
 		}
-
-		// 다음 필터로 넘김
-		System.out.println("[필터] 로그인 확인됨. 로그인 필터 통과.");
-		chain.doFilter(request, response);
 
 	}
 
