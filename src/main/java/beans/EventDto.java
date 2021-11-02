970d15e5fda471933c29ee4ade6dde15fc4c2ff7
soeun407 <88871773+soeun407@users.noreply.github.com>
2021-11-03 03:24:46 +0900

    Update EventDto.java

--- src/main/java/beans/EventDto.java
+++ src/main/java/beans/EventDto.java
@@ -1,5 +1,63 @@
 package beans;
 
 public class EventDto {
+	
+	private int eventIdx;
+	private int usersIdx;
+	private String eventName;
+	private String eventDetail;
+	private String eventDate;
+	private String eventCountView;
+	private String eventCountReply;
+	
+	public int getEventIdx() {
+		return eventIdx;
+	}
+	public void setEventIdx(int eventIdx) {
+		this.eventIdx = eventIdx;
+	}
+	public int getUsersIdx() {
+		return usersIdx;
+	}
+	public void setUsersIdx(int usersIdx) {
+		this.usersIdx = usersIdx;
+	}
+	public String getEventName() {
+		return eventName;
+	}
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+	public String getEventDetail() {
+		return eventDetail;
+	}
+	public void setEventDetail(String eventDetail) {
+		this.eventDetail = eventDetail;
+	}
+	public String getEventDate() {
+		return eventDate;
+	}
+	public void setEventDate(String eventDate) {
+		this.eventDate = eventDate;
+	}
+	public String getEventCountView() {
+		return eventCountView;
+	}
+	public void setEventCountView(String eventCountView) {
+		this.eventCountView = eventCountView;
+	}
+	public String getEventCountReply() {
+		return eventCountReply;
+	}
+	public void setEventCountReply(String eventCountReply) {
+		this.eventCountReply = eventCountReply;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "EventDto [eventIdx=" + eventIdx + ", usersIdx=" + usersIdx + ", eventName=" + eventName
+				+ ", eventDetail=" + eventDetail + ", eventDate=" + eventDate + ", eventCountView=" + eventCountView + ", eventCountReply=" + eventCountReply + "]";
+	}
 
 }
