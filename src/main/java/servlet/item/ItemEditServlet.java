138dd2aecf2e33df9e0dbac743c36ca3123e5713
hongbba <88869198+hongbba@users.noreply.github.com>
2021-11-05 21:57:01 +0900

    관광지 게시물 수정 구현 완료

--- src/main/java/servlet/item/ItemEditServlet.java
+++ src/main/java/servlet/item/ItemEditServlet.java
@@ -21,8 +21,28 @@ public class ItemEditServlet extends HttpServlet{
 		try {
 
 			
-			//수정 서블릿
+			ItemDto itemDto = new ItemDto();
+			itemDto.setItemType(req.getParameter("itemType"));
+			itemDto.setItemName(req.getParameter("itemName"));
+			itemDto.setItemDetail(req.getParameter("itemDetail"));
+			itemDto.setItemPeriod(req.getParameter("itemPeriod"));
+			itemDto.setItemTime(req.getParameter("itemTime"));
+			itemDto.setItemHomepage(req.getParameter("itemHomepage"));
+			itemDto.setItemParking(req.getParameter("itemParking"));
+			itemDto.setItemAddress(req.getParameter("itemAddress"));
+			itemDto.setItemIdx(Integer.parseInt(req.getParameter("itemIdx")));
 			
+			ItemDao itemDao = new ItemDao();
+			boolean result = itemDao.update(itemDto);
+			
+			//수정 성공시 상세 페이지로 이동
+			if(result) {
+				resp.sendRedirect("detail.jsp?itemIdx="+itemDto.getItemIdx());	
+			}
+			//수정 실패시 에러페이지 500번으로
+			else {
+				resp.sendError(500);
+			}
 
 		}
 		catch(Exception e) {
