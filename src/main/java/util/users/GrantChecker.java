d28a32c9599916dcd94acce496de447c114339e6
Daanta <daanta@naver.com>
2021-11-06 21:52:47 +0900

    211106_2152 권한검사 클래스 코드 간결화

--- src/main/java/util/users/GrantChecker.java
+++ src/main/java/util/users/GrantChecker.java
@@ -1,57 +1,64 @@
 package util.users;
 
-import beans.UsersDao;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 public class GrantChecker {
 
-	// 권한검사: 내가 나에 대해서 요청한 것이거나, 아니면 내가 관리자여야만 true를 반환함
-	// 글 수정/삭제, 회원정보 수정/탈퇴 등에 사용
-	public static boolean isGranted(String sessionId, String targetId, String sessionGrade) {
+	private GrantChecker() { super(); }
 
-		// 1. 관리자 권한이라면 무조건 true를 회신함
-		boolean isAdmin = sessionGrade != null && sessionGrade.equals(UsersUtils.GRADE_ADMIN);
-		if(isAdmin) return true;
+	// 권한검사: 현재 세션의 유저에게 타겟id 데이터의 조작권한이 있는지 검사.
+	//         권한 있으면 true 아니면 false를 리턴함.
+	// true가 나오려면, (1) 요청자가 관리자이거나 (2) 내가 내 id에 요청하는 경우여야 함.
+	public static boolean isGranted(String sessionId, String sessionGrade, String targetId) throws Exception {
 
-		// 2. 내가 관리자가 아니라면 여기로 온다.
-		//    그럼 적어도 내가 나에 대해 요청한 것인지 확인. 맞으면 true 돌려줌.
-		boolean isSelf = sessionId != null && sessionId.equals(targetId);
-		if(isSelf) return true;
+		// 1. 세션 검사: 로그인했는지 검사
+		System.out.print("[권한확인] 1. 세션 검사.. ");
+		boolean sessionExists = sessionId != null && !sessionId.equals("");
+		if(!sessionExists) {
+			System.out.println("세션이 없습니다. 따라서 아무 권한도 줄 수 없습니다.");
+			return false;
+		} else {
+			System.out.println("세션 존재 확인 완료.");
+		}
 
-		// 3. 위 내용 둘다 아니라면 여기로 온다.
-		//    내가 관리자도 아니고, 대상id가 내 스스로도 아니므로, false를 회신
+		// 2. 요청자가 관리자인 경우, 무조건 프리패스 (바로 true 리턴)
+		System.out.println("[권한확인] 2. 관리자 여부 검사.. ");
+		boolean isAdmin = sessionGrade.equals(UsersUtils.GRADE_ADMIN);
+		if(isAdmin) {
+			System.out.println(UsersUtils.GRADE_ADMIN + " 등급입니다. 권한 확인되었습니다.");
+			return true;
+		} else {
+			System.out.println("관리자가 아닙니다.");
+		}
+
+		// 3. 본인이면, 즉 요청자 id와 요청대상 id가 같으면 true
+		System.out.print("[권한확인] 3. 본인 확인.. ");
+		boolean isSelf = sessionId.equals(targetId);
+		if(isSelf) {
+			System.out.println("본인 확인되었습니다. 권한 확인되었습니다.");
+			return true;
+		} else {
+			System.out.println("본인 ID에 대한 요청이 아닙니다. (세션 ID = '" + sessionId + "' ↔ 대상id = '" + targetId + "'");
+		}
+
+		// 4. 관리자가 요청한 것도 아니고 자기 자신에 대해 요청한 것도 아니라면,
+		//    당연히 권한이 없으므로, false 회신
+		System.out.println("[권한확인] 4. 권한이 없는 사용자임이 확인되었습니다.");
 		return false;
 
 	}
 
-	// 권한검사: 데이터 조작 권한이 있는지 확인하여 t/f로 리턴해 줌.
-	// true가 나오려면, 요청자가 관리자이거나, 아니면 요청대상 ID가 본인의 ID여야 함.
-	public static boolean isGranted(String sessionId, String targetId, UsersDao dao) throws Exception {
 
-		// 1. 세션 검사: 누가 요청했든 간에, 일단 요청자의 세션이 있어야 됨 즉 로그인된 사람이 요청해야 됨
-		System.out.print("[권한확인] 1. 세션 검사..");
-		boolean sessionExists = sessionId != null;
-		if(!sessionExists) return false;
-		System.out.println("세션 확인.");
+	// 오버라이드 메소드들
 
-		// 세션 id에 따른 권한여부를 검사함.
-		// 2. 요청자가 관리자인 경우, 프리패스. 4 생략하고 바로 5로 간다.
-		System.out.println("[권한확인] 2. 관리자 여부 검사");
-		boolean isAdmin = dao.get(sessionId).getUsersGrade().equals("관리자");
-		if(isAdmin) System.out.println("관리자입니다.");
-
-		// 3. 요청자가 관리자가 아닌, 경우 본인에 대한 요청인지 확인
-		else {
-			System.out.println("관리자가 아닙니다.");
-			System.out.print("[권한확인] 3. 본인 확인..");
-			boolean isSelf = sessionId.equals(usersId);
-			if(!isSelf) return false;
-			System.out.println("본인 확인됨.");
-		}
-
-		// 4. 모든 절차를 통과했으면 true 회신
-		System.out.print("[권한확인] 4. 적절한 권한이 확인되었습니다.");
-		return true;
-
+	// (request, 대상id) 형태
+	public static boolean isGranted(HttpServletRequest req, String targetId) throws Exception {
+		return isGranted(req.getSession(), targetId);
+	}
+	// (session, 대상id) 형태
+	public static boolean isGranted(HttpSession session, String targetId) throws Exception {
+		return isGranted((String)session.getAttribute("usersId"), (String)session.getAttribute("usersGrade"), targetId);
 	}
 
 }
