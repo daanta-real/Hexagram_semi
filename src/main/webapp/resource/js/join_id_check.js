83464832efbf018c04664995b28216d527527001
seona_min <88869283+seonamin@users.noreply.github.com>
2021-11-15 10:13:58 +0900

    회원가입 아이디 중복검사 빈값 여부 추가

--- src/main/webapp/resource/js/join_id_check.js
+++ src/main/webapp/resource/js/join_id_check.js
@@ -9,35 +9,40 @@
     $(function(){
         $("input[name=usersId]").on("blur", function(){
             var inputId = $(this).val();
-            $.ajax({
-                //준비 설정
-                //요청을 전달할 주소
-                url:"http://localhost:8080/Hexagram_semi/users/join_id_check.nogari",
-                type:"get", //전송방식 post로도 가능
-                data:{ //전송시 첨부할 파라미터 정보
-                    usersId:inputId  //이름:값
-                },  
-
-                //완료 처리
-                success:function(resp){
-                    console.log("ID 중복검사 요청 성공");
-                    console.log("ID : "+resp);
-                    if(resp == "CAN_USE"){
-                        $("input[name=usersId]").next().text("아이디 사용 가능");
-						//사용가능한 아이디라면 다른 입력창에 대한 입력이 가능
-						$("input").prop("disabled",false);
-                    }
-                    else if(resp =="USED"){
-                        $("input[name=usersId]").next().text("아이디가 이미 사용중입니다");
-						//아이디가 중복이라면 다른 입력창에 대한 입력을 방지
-						$("input").not($("input[name=usersId]")).prop("disabled",true);
-                    }
-
-                },
-                error:function(err){ //통신 실패
-                    console.log("ID 중복검사 요청 실패");
-                    console.log(err);
-                }
-            });
+			if(inputId != ""){ //아이디 입력값이 빈값이 아니라면
+	            $.ajax({
+	                //준비 설정
+	                //요청을 전달할 주소
+	                url:"http://localhost:8080/Hexagram_semi/users/join_id_check.nogari",
+	                type:"get", //전송방식 post로도 가능
+	                data:{ //전송시 첨부할 파라미터 정보
+	                    usersId:inputId  //이름:값
+	                },  
+					
+	                //완료 처리
+	                success:function(resp){
+	                    console.log("ID 중복검사 요청 성공");
+	                    console.log("ID : "+resp);
+	                    if(resp == "CAN_USE"){
+	                        $("input[name=usersId]").next().text("아이디 사용 가능");
+							//사용가능한 아이디라면 다른 입력창에 대한 입력이 가능
+							$("input").prop("disabled",false);
+	                    }
+	                    else if(resp =="USED"){
+	                        $("input[name=usersId]").next().text("아이디가 이미 사용중입니다");
+							//아이디가 중복이라면 다른 입력창에 대한 입력을 방지
+							$("input").not($("input[name=usersId]")).prop("disabled",true);
+	                    }
+	
+	                },
+	                error:function(err){ //통신 실패
+	                    console.log("ID 중복검사 요청 실패");
+	                    console.log(err);
+	                }
+	            });
+			}
+			else{ // 아이디 입력값이 빈칸이라면
+				  $("input[name=usersId]").next().text("아이디를 입력해 주세요");
+			}
         });
     });
