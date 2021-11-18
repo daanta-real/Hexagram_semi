b5aef5ca02accbfb70ddc8f50c3c61125c70b48a
seona_min <88869283+seonamin@users.noreply.github.com>
2021-11-18 22:16:32 +0900

    오타 수정

--- src/main/webapp/resource/js/regexPw.js
+++ src/main/webapp/resource/js/regexPw.js
@@ -7,16 +7,16 @@ window.addEventListener("load", () => {
     document.querySelector(".form-regexCheck input[name=usersPw]").addEventListener("blur", function(){
 
         var form  = document.querySelector('.form-regexCheck');
-        var regex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[-_~!@#$%^&*=+/,.;’”?])[a-zA-Z0-9-_~!@#$%^&*=+/,.;’”?]{4,20}$/
+        var regex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[-_~!@#$%^&*=+/,.;’”?])[a-zA-Z0-9-_~!@#$%^&*=+/,.;’”?]{4,20}$/;
         var inputPw = form.querySelector("input[name=usersPw]").value;
-        var message = form.querySelector("input[name=usersPw] + .message");
+        var message = form.querySelector("input[name=usersPw]+.message");
         if(inputPw != ""){
-            if(!regex.test(inputPw)){
-                console.log("비밀번호 정규표현식 검사 실패");
-                message.textContent = "비밀번호는 영문, 숫자, 특수문자 _-~!@#$%^&*=+/,.;’”? 하나씩 포함되야 합니다";
-            }else{
+            if(regex.test(inputPw)){
                 console.log("비밀번호 정규표현식 검사 통과");
                 message.textContent = "";
+            }else{
+                console.log("비밀번호 정규표현식 검사 실패");
+                message.textContent = "비밀번호는 영문, 숫자, 특수문자 _-~!@#$%^&*=+/,.;’”? 하나씩 포함되야 합니다";
             }		
         }else{
             console.log("비밀번호 미입력");
