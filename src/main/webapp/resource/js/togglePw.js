6a85cf350935026bfb12929b8ae7f0b88dc2242e
seona_min <88869283+seonamin@users.noreply.github.com>
2021-11-18 22:16:57 +0900

    오타 수정

--- src/main/webapp/resource/js/togglePw.js
+++ src/main/webapp/resource/js/togglePw.js
@@ -1,10 +1,12 @@
 
 
+
+
 // 로드 이후 리스너 추가
 window.addEventListener("load", () => {
    
    // 비밀번호 숨김/보기 토글
-   document.querySelector(".form-regexCheck input[type=checkbox]").addEventListener("input", function() {
+   document.querySelector(".form-regexCheck .togglePw").addEventListener("input", function() {
     
         //type이 password면 text로 바꾸고 text면 password로 바꾼다
         var form = document.querySelector('.form-regexCheck');
@@ -12,14 +14,9 @@ window.addEventListener("load", () => {
         if (inputPw.type == "password") {
             inputPw.type = "text";
         } else {
-            input.type = "password"
+            inputPw.type = "password";
         }
-        var updatePw = form.querySelector("input[name=pwUpdate]");
-        if (updatePw.type == "password") {
-            updatePw.type = "text";
-        } else {
-            updatePw.type = "password"
-        }
+      
     });	
    
 });
