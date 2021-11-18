44d3a6f9f13fc2e0976a9f40ce6bcd643a980100
Daanta <daanta@naver.com>
2021-11-18 12:18:18 +0900

    211118_1217 로그아웃 요청 시 조건 따지지 않고 반드시 실행되도록 변경

--- src/main/java/servlet/users/UsersLogoutServlet.java
+++ src/main/java/servlet/users/UsersLogoutServlet.java
@@ -25,17 +25,14 @@ public class UsersLogoutServlet extends HttpServlet {
 			String sessionId = Sessioner.getUsersId(session);
 			System.out.print("[회원 로그아웃] 0. 현재 sessionId = " + sessionId);
 
-			// 1. 기 로그아웃 검사: 이미 로그아웃되어 session의 id가 없으면 에러처리
-			System.out.print("[회원 로그아웃] 1. 로그아웃 검사(이미 로그아웃 되어있는지 검사)..");
-			if(!Sessioner.isLoggedIn(session)) throw new Exception();
-			System.out.println("아직 로그아웃되어 있지 않은 게 확인되었으므로 로그아웃 처리를 정상적으로 수행하겠습니다.");
+			// 1. 기 로그아웃 검사는 하지 않는다. 무조건 로그아웃시켜야 하는 경우도 있을 수 있으므로 없앴다.
 
 			// 2. 로그아웃 처리
-			System.out.print("[회원 로그아웃] 2. 로그아웃 처리..");
+			System.out.print("[회원 로그아웃] 1. 로그아웃 처리..");
 			Sessioner.logout(session);
 
 			// 3. 로그아웃을 성공적으로 끝냈으면 메인 페이지로 리다이렉트
-			System.out.println("[회원 로그아웃] 3. 세션 비우기 완료.");
+			System.out.println("[회원 로그아웃] 2. 세션 비우기 완료.");
 			resp.sendRedirect(req.getContextPath()+"/index.jsp");
 
 		}
