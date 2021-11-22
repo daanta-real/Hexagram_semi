caa9e951508c88810eec7427207db8a118c93c5f
Daanta <daanta@naver.com>
2021-11-23 08:33:44 +0900

    211123_0833 EventDto 사양 고정

--- src/main/java/beans/EventDto.java
+++ src/main/java/beans/EventDto.java
@@ -1,93 +1,46 @@
 package beans;
 
-import java.util.Date;
-import java.util.StringTokenizer;
-
 public class EventDto {
 
-	private int eventIdx;
-	private int usersIdx;
+	// 1. Declarations
+	private Integer eventIdx;
+	private Integer usersIdx;
 	private String eventName;
 	private String eventDetail;
-	private Date eventDate;
+	private java.util.Date eventDate;
 	private Integer eventCountView;
 	private Integer eventCountReply;
-	private String eventAddress;
 
-	public int getEventIdx() {
-		return eventIdx;
-	}
-	public void setEventIdx(int eventIdx) {
-		this.eventIdx = eventIdx;
-	}
-	public int getUsersIdx() {
-		return usersIdx;
-	}
-	public void setUsersIdx(int usersIdx) {
-		this.usersIdx = usersIdx;
-	}
-	public String getEventName() {
-		return eventName;
-	}
-	public void setEventName(String eventName) {
-		this.eventName = eventName;
-	}
-	public String getEventDetail() {
-		return eventDetail;
-	}
-	public void setEventDetail(String eventDetail) {
-		this.eventDetail = eventDetail;
-	}
-	public Date getEventDate() {
-		return eventDate;
-	}
-	public void setEventDate(Date eventDate) {
-		this.eventDate = eventDate;
-	}
-	public Integer getEventCountView() {
-		return eventCountView;
-	}
-	public void setEventCountView(Integer eventCountView) {
-		this.eventCountView = eventCountView;
-	}
-	public Integer getEventCountReply() {
-		return eventCountReply;
-	}
-	public void setEventCountReply(Integer eventCountReply) {
-		this.eventCountReply = eventCountReply;
-	}
-	
-//댓글 수 갱신 기능
+	// 2. Constructors
+	public EventDto() { super(); }
+
+	// 3. Getters
+	public Integer getEventIdx() { return eventIdx; }
+	public Integer getUsersIdx() { return usersIdx; }
+	public String getEventName() { return eventName; }
+	public String getEventDetail() { return eventDetail; }
+	public java.util.Date getEventDate() { return eventDate; }
+	public Integer getEventCountView() { return eventCountView; }
+	public Integer getEventCountReply() { return eventCountReply; }
+
+	// 4. Setters
+	public void setEventIdx(Integer eventIdx) { this.eventIdx = eventIdx; }
+	public void setUsersIdx(Integer usersIdx) { this.usersIdx = usersIdx; }
+	public void setEventName(String eventName) { this.eventName = eventName; }
+	public void setEventDetail(String eventDetail) { this.eventDetail = eventDetail; }
+	public void setEventDate(java.util.Date eventDate) { this.eventDate = eventDate; }
+	public void setEventCountView(Integer eventCountView) { this.eventCountView = eventCountView; }
+	public void setEventCountReply(Integer eventCountReply) { this.eventCountReply = eventCountReply; }
+
+	// 5. Status Information Methods
 	public boolean isCountReply() {
 		return this.eventCountReply > 0;
 	}
-		
-	public String getAdressCity() {
-		StringTokenizer st = new StringTokenizer(this.eventAddress," ");
-		StringBuffer sb = new StringBuffer();
-		int i = 0;
-		while(st.hasMoreTokens()) {
-			i++;
-			sb.append(st.nextToken());
-			if(i==1) break;
-			}
-		return sb.toString();
+	@Override
+	public String toString() {
+		return "EventDto [eventCountReply=" + eventCountReply + ", eventCountView="
+				+ eventCountView + ", eventDate=" + eventDate + ", eventDetail=" + eventDetail + ", eventIdx="
+				+ eventIdx + ", eventName=" + eventName + ", usersIdx=" + usersIdx + "]";
 	}
-			
-	public String getAdressCitySub() {
-			StringTokenizer st = new StringTokenizer(this.eventAddress," ");
-			StringBuffer sb = new StringBuffer();
-			int i = 0;
-			while(st.hasMoreTokens()) {
-				i++;
-				if(i==1) st.nextToken();
-				if(i==2) sb.append(st.nextToken());
-				if(i==2) break;
-			}
-			return sb.toString();
-	}
-		
-	public EventDto() {
-		super();
-	}
+
 }
