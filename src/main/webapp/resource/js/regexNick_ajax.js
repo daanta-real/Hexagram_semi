d1e8e75deeffd8ec4c9fba8f2f2d5a3353bd4ce4
seona_min <88869283+seonamin@users.noreply.github.com>
2021-11-22 19:19:56 +0900

    정규식 검사 실패, 닉네임 중복시 submit버튼 비활성화

--- src/main/webapp/resource/js/regexNick_ajax.js
+++ src/main/webapp/resource/js/regexNick_ajax.js
@@ -2,10 +2,17 @@
 //- 입력값이 있을 경우에만 검사하도록 설정. 미입력시 'OOO을 입력해 주세요' 메세지 출력
 //     - 닉네임 정규표현식 검사 통과 후 닉네임 중복검사 진행
 
- 
-   function regexNick_ajax(){
 
-	    // NICK 정규표현식 검사 
+ // 로드 이후 리스너 추가
+window.addEventListener("load", () => {
+ 
+    // NICK 정규표현식 검사 
+    document.querySelector(".form-regexCheck input[name=usersNick]").addEventListener("blur", function(){
+    	// 정규식 검사 실패, 중복, 미입력시 submit 버튼 비활성화
+		// submit.disabled = true; 이면 비활성화
+		// submit.disabled = false; 이면 활성화
+		var submit = document.querySelector("input[type=submit]"); 
+		
         var form  = document.querySelector('.form-regexCheck');
         var regex = /^[a-zA-Zㄱ-ㅎ가-힣0-9]{2,10}$/;
         var inputNick = form.querySelector("input[name=usersNick]").value;
@@ -36,12 +43,10 @@
 					console.log("NickName 중복검사 요청 성공. 닉네임 : "+resp);
 					if(resp == "CAN_USE") { //사용가능한 닉네임라면 다른 입력창에 대한 입력이 가능
 						$(message).text("닉네임 사용 가능");
-						$("input").prop("disabled", false);
-						return true;
+						submit.disabled = false;
 					} else if(resp =="USED") { //닉네임이 중복이라면 다른 입력창에 대한 입력을 방지
 						$(message).text("닉네임이 이미 사용중입니다");
-						$("input").not($("input[name=usersNick]")).prop("disabled",true);
-						return false;
+						submit.disabled = true;
 					}
 				}
 				
@@ -57,12 +62,13 @@
             }else{
                 console.log("닉네임 정규표현식 검사 실패");
                 message.textContent = "영문, 한글, 숫자 2~10글자로 작성해주세요";
-				return false;
+				submit.disabled = true;
             }
         }else{
             console.log("닉네임 미입력");
             message.textContent = "닉네임을 입력해 주세요";
-			return false;
+			submit.disabled = true;
         }
- }
+    });
 
+});
