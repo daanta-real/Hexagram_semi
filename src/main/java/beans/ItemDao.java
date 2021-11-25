94db609ff901cbb78b9382d217d35b0784396d34
dolddi6778@naver.com <dolddi6778@naver.com>
2021-11-25 12:32:09 +0900

    계절별 축제 항목 리스트에 추가

--- src/main/java/beans/ItemDao.java
+++ src/main/java/beans/ItemDao.java
@@ -596,5 +596,48 @@ public class ItemDao implements PaginationInterface<ItemDto> {
 
 			return list;
 		}
+		
+		//계절별로 축제 리스트를 뽑아오는 메소드
+		public List<ItemDto> getSeasonItem(String first,String second,String third) throws Exception {
+
+			Connection con = JdbcUtils.connect3();
+			String sql = "select * from"
+					+ "(select rownum rn,tmp.* from("
+					+ "select * from item"
+					+ " where item_type = '축제'"
+					+ " and SUBSTR(item_period, 6, 2) in (?,?,?) order by item_count_view desc"
+					+ ")tmp)where rn between 1 and 5";
+
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, first);
+			ps.setString(2, second);
+			ps.setString(3, third);
+			
+			ResultSet rs = ps.executeQuery();
+			List<ItemDto> list = new ArrayList<>();
+			while (rs.next()) {
+				ItemDto itemDto = new ItemDto();
+				itemDto.setItemIdx(rs.getInt("item_idx"));
+				itemDto.setUsersIdx(rs.getInt("users_idx"));
+				itemDto.setItemType(rs.getString("item_type"));
+				itemDto.setItemName(rs.getString("item_name"));
+				itemDto.setItemDetail(rs.getString("item_detail"));
+				itemDto.setItemPeriod(rs.getString("item_period"));
+				itemDto.setItemTime(rs.getString("item_time"));
+				itemDto.setItemHomepage(rs.getString("item_homepage"));
+				itemDto.setItemParking(rs.getString("item_parking"));
+				itemDto.setItemAddress(rs.getString("item_address"));
+				itemDto.setItemDate(rs.getDate("item_date"));
+				itemDto.setItemCountView(rs.getInt("item_count_view"));
+				itemDto.setItemCountReply(rs.getInt("item_count_reply"));
+
+
+				list.add(itemDto);
+			}
+
+			con.close();
+
+			return list;
+		}
 
 }
