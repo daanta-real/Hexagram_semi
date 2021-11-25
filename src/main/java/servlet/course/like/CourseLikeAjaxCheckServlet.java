c5322a68934dcef265e4a4837fee461a5635dbcd
dolddi6778@naver.com <dolddi6778@naver.com>
2021-11-25 16:30:31 +0900

    코스 좋아요 구현 중

---
+++ src/main/java/servlet/course/like/CourseLikeAjaxCheckServlet.java
@@ -0,0 +1,9 @@
+package servlet.course.like;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/course/insert_ajax_course_like.nogari")
+public class CourseLikeAjaxCheckServlet extends HttpServlet {
+
+}
