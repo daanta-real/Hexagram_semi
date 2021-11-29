40bc24555b1692caa56deac916c3e241bb1c13cd
seona_min <88869283+seonamin@users.noreply.github.com>
2021-11-29 10:04:30 +0900

    초기화 스크립트 모듈 선택자 수정

--- src/main/webapp/resource/js/resetAll.js
+++ src/main/webapp/resource/js/resetAll.js
@@ -6,8 +6,8 @@ window.addEventListener("load", () => {
 	// 초기화 버튼 클릭시 검사관련 메세지 초기화 하기
  	// = 모든 이벤트도 제거
 	// = 새로고침으로 해결
-	document.querySelector(".form-regexCheck .reset").addEventListener("click", function(){
-		 console.log("회원가입 form 초기화 시작");
+	document.querySelector(".reset").addEventListener("click", function(){
+		 console.log("form 초기화 시작");
 		 location.reload();
 	});
 	
